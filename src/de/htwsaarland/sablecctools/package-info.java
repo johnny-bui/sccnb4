4135c91fadd00ef5f8ed34ce246db021cfdd1a75
hbui <>
2012-07-08 14:25:52 +0200

    chose sablecc and sablecc4 as file extentsion for sablecc v4

--- src/de/htwsaarland/sablecctools/package-info.java
+++ src/de/htwsaarland/sablecctools/package-info.java
@@ -2,8 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-@TemplateRegistration(folder = "Other", displayName = "#SableCC4template_displayName", content = "SableCC4Template.sablecc4")
-@Messages(value = "SableCC4template_displayName=Empty SableCC4 file")
+@TemplateRegistration(folder = "SableCC", displayName = "#SableCC4template_displayName", content = "SableCC4Template.sablecc4")
+@Messages(value = "SableCC4template_displayName=SableCC4 file")
 package de.htwsaarland.sablecctools;
 
 import org.netbeans.api.templates.TemplateRegistration;
