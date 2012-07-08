046c57d7acc81f719f9470c3c0c3657372ea1e3f
hbui <>
2012-07-08 08:43:27 +0200

    improve color

--- src/de/htwsaarland/sablecctools/SCC4Language.java
+++ src/de/htwsaarland/sablecctools/SCC4Language.java
@@ -15,7 +15,7 @@ import org.netbeans.modules.csl.spi.LanguageRegistration;
  * @author phucluoi
  * @version May 17, 2012
  */
-//@LanguageRegistration(mimeType = "text/x-sablecc4")
+@LanguageRegistration(mimeType = "text/x-sablecc4")
 public class SCC4Language extends DefaultLanguageConfig
 {
 
@@ -27,7 +27,7 @@ public class SCC4Language extends DefaultLanguageConfig
 
 	@Override
 	public String getDisplayName() {
-		return "UIL";
+		return "SableCC4";
 	}
 
 }
